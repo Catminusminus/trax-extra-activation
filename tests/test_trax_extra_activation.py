f7476c01176a4389646c3307ffe957e1d1e93306
Catminusminus <getomya@svk.jp>
2021-06-26 16:52:46 +0900

    remove unused functions in tests

--- tests/test_trax_extra_activation.py
+++ tests/test_trax_extra_activation.py
@@ -1,18 +1,6 @@
 from trax import layers as tl
 from trax.fastmath import numpy as jnp
-from trax_extra_activation import (
-    Celu,
-    HardShrink,
-    HardSwish,
-    LogSigmoid,
-    Mish,
-    Relu6,
-    Silu,
-    SoftShrink,
-    SoftSign,
-    TanhExp,
-    TanhShrink,
-)
+from trax_extra_activation import Celu
 
 
 def test_celu() -> None:
